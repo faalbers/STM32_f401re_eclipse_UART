46a436f6ad98fcbd0c31db086c8877746d86b5c0
faalbers <chalbers@gmail.com>
2015-09-22 11:48:57 -0700

    first push

---
+++ src/main.c
@@ -0,0 +1,135 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+#include "stm32f4xx.h"
+
+
+// ----------------------------------------------------------------------------
+//
+// Standalone STM32F4 empty sample (trace via DEBUG).
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the DEBUG output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+/* UART handler declaration */
+UART_HandleTypeDef UartHandle;
+
+
+/* Private function prototypes -----------------------------------------------*/
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/* Private function prototypes -----------------------------------------------*/
+static void Error_Handler(void);
+
+int
+main(int argc, char* argv[])
+{
+  // At this stage the system clock should have already been configured
+  // at high speed.
+
+  // Setup LED
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  // Enable GPIOA Peripheral clock
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+  // Configure pin in output push/pull mode
+  GPIO_InitStructure.Pin = GPIO_PIN_5;
+  GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+  GPIO_InitStructure.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  // Setup UART
+
+  // Enable GPIOA Peripheral clock
+  RCC->AHB2ENR |= RCC_APB2ENR_USART6EN;
+
+  UartHandle.Instance          = USART6;
+
+  UartHandle.Init.BaudRate     = 9600;
+  UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
+  UartHandle.Init.StopBits     = UART_STOPBITS_1;
+  UartHandle.Init.Parity       = UART_PARITY_ODD;
+  UartHandle.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
+  UartHandle.Init.Mode         = UART_MODE_TX_RX;
+  UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+
+  if(HAL_UART_Init(&UartHandle) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+  // Turn LED ON
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+
+  /* Output a message on Hyperterminal using printf function */
+  //printf("\n\r UART Printf Example: retarget the C library printf function to the UART\n\r");
+  //printf("abcd");
+
+  char ch = 'a';
+  //while(HAL_UART_GetState(&UartHandle) == HAL_UART_STATE_RESET);
+  HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF);
+
+  // Turn LED ON
+  //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  // Infinite loop
+  while (1) {
+       // Add your code here.
+  }
+}
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
+
+static void Error_Handler(void)
+{
+  while(1)
+  {
+  }
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
